6332e640a628a56a0e7dd23a669e3e24a433239e
austinw8 <austinw.mangelson@gmail.com>
2024-09-24 13:59:32 -0600

    tidy tuesday challenges

---
+++ 2024/2024-09-17/shakespeare_dialogue.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+hamlet <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-09-17/hamlet.csv')
+macbeth <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-09-17/macbeth.csv')
+romeo_juliet <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-09-17/romeo_juliet.csv')
+
+#longest dialogue spoken by one single character
